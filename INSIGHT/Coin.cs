ea010da179a9434bf9c86c81c87b92c81953b2aa
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2021-10-29 16:11:03 -0400

    Initial Commit

---
+++ INSIGHT/Coin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSIGHT
+{
+    class Coin
+    {
+        public int Value { get; } = 0;
+        public string Initial { get; }
+        public int Count { get; set; } = 0;
+
+        public Coin(int value, string initial)
+        {
+            Value = value;
+            Initial = initial;
+        }
+
+        public int CalculateCount(int cents)
+        {
+            if (cents / Value > 0)
+            {
+                Count = cents / Value;
+                cents -= Count * Value;
+            }
+
+            return cents;
+        }
+
+        public string PrintCount()
+        {
+            if (Count > 0)
+            {
+                return Count + Initial;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ INSIGHT/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace INSIGHT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 100; i++)
+            {
+                int centsRemaining = i;
+
+                List<Coin> coins = new List<Coin>();
+                coins.Add(new Coin(25, "Q"));
+                coins.Add(new Coin(10, "D"));
+                coins.Add(new Coin(5, "N"));
+                coins.Add(new Coin(1, "P"));
+
+                foreach (Coin coin in coins)
+                {
+                    centsRemaining = coin.CalculateCount(centsRemaining);
+                    Console.Write(coin.PrintCount() + "\t");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
